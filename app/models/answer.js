1bd7c22ef6eebd26eab05444d5d6a4cb1c835507
Susha Dore <susha.dore@gmail.com>
2017-06-02 15:35:05 -0700

    add answer creation

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
-  notes: DS.attr(),
   date: DS.attr(),
-  answers: DS.hasMany("answer", { async: true })
+  question: DS.belongsTo("question", { async: true })
 });
