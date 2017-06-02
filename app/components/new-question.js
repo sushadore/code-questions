a520ec4f518257b171c47785873deb1c06a1b22c
Susha Dore <susha.dore@gmail.com>
2017-06-02 12:11:20 -0700

    add question creation

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set("addNewQuestion", true);
+    },
+    saveQuestion() {
+      var params = {
+        content: this.get("content"),
+        author: this.get("author"),
+        notes: this.get("notes"),
+        date: this.get("date")
+      };
+      this.set("addNewQuestion", false);
+      this.sendAction("saveQuestion", params);
+    }
+  }
+});
